928b3f40309fbe81944a6f440935bbe68236cd55
Plutonist <widesense.plqws@gmail.com>
2017-08-15 14:05:48 +0800

    Update main.js

--- lolibooru/main.js
+++ lolibooru/main.js
@@ -8,13 +8,15 @@ export default {
     const raw = await url.get().raw()
     const covers = raw.match(/Preload.preload\('.*'\);/g).map(l => l.match(/Preload.preload\('(.*)'\);/)[1])
     const $ = raw.cheer()
-    const rs = $('table.highlightable tbody tr').map((i, el) => ({
-      title: $(el).find('a').text(),
-      cover: covers[i],
-      by: $(el).find('td').eq(1).text(),
-      date: $(el).find('td').eq(3).text(),
-      url: $(el).find('a').attr('href').relativeTo(url)
-    })).get()
+    const rs = $('table.highlightable tbody tr')
+      .filter((i, el) => parseInt($(el).find('td').eq(2).text()) > 0)
+      .map((i, el) => ({
+        title: $(el).find('a').text(),
+        cover: covers[i],
+        by: $(el).find('td').eq(1).text(),
+        date: $(el).find('td').eq(3).text(),
+        url: $(el).find('a').attr('href').relativeTo(url)
+      })).get()
     posts.push(...rs)
     const href = $('.nextPage').attr('href')
     if (href) {
